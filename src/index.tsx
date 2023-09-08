b443d9e3c9862e0aa847fa360d1e4f585ebfbd49
owen970517 <tamia123123@naver.com>
2023-09-08 19:20:16 +0900

    style: reset.css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {  RouterProvider } from 'react-router-dom';
 import { Router } from './Router';
+import './styles/reset.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
