8e78371169034d1737fc80f8a35651d0ace5714f
owen970517 <tamia123123@naver.com>
2023-09-08 19:38:33 +0900

    design: 디자인 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import SearchBar from './SearchBar';
 import SearchPreview from '../containers/SearchPreview';
+import SearchBarContainer from '../containers/SearchBarContainer';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <>
-      <SearchBar/>
-      <SearchPreview/>
-    </>
+    <HomeContainer>
+      <HomeHeader>
+        국내 모든 임상실험 검색하고 <br /> 온라인으로 참여하기
+      </HomeHeader>
+      <SearchSection>
+        <SearchBarContainer/>
+        <SearchPreview/>
+      </SearchSection>
+    </HomeContainer>
   )
 }
 
+const HomeContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+`;
+
+const HomeHeader = styled.h1`
+    text-align: center;
+    font-size: 40px;
+    font-weight: 800;
+    line-height: 1.6;
+    margin-bottom: 20px;
+`;
+const SearchSection = styled.section`
+
+`;
 export default Home
