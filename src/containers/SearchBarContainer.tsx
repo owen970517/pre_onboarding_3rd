332554820f9f0ae3ca8012e4b41f43c3647cf3c4
owen970517 <tamia123123@naver.com>
2023-09-08 19:28:28 +0900

    refactor: searchIcon 추가 및 코드 수정

--- src/containers/SearchBarContainer.tsx
+++ src/containers/SearchBarContainer.tsx
@@ -1,22 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useInput } from '../hooks/useInput'
+import SearchIcon from '../components/SearchIcons';
 
 const SearchBarContainer = () => {
-  const { searchValue, handleSearchValue, handleInputFocus, handleInputBlur,handleSearched } = useInput();
+  const { searchValue, handleSearchValue, handleInputFocus,handleSearched } = useInput();
 
   return (
-    <Wrapper onSubmit={(e) => handleSearched(e)}>
-      <input placeholder='질환명을 입력하시오' type='text' value ={searchValue} onChange={(e) => handleSearchValue(e.target.value)} onFocus={handleInputFocus} onBlur={handleInputBlur}/>
-      <button>검색</button>
-    </Wrapper>
+    <SearchBar onSubmit={(e) => handleSearched(e)}>
+      <SearchIcon size={16} />
+      <input placeholder='질환명을 입력하시오' type='text' value ={searchValue} onChange={(e) => handleSearchValue(e.target.value)} onFocus={handleInputFocus} />
+      <button type='submit'>
+        <SearchIcon size={16} />
+      </button>
+    </SearchBar>
   )
 }
 
-const Wrapper = styled.form`
-  display :flex;
-  justify-content: center;
+const SearchBar = styled.form`
+  width: 490px;
+  height: 75px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
+  padding: 10px 10px 10px 25px;
+  border-radius: 50px;
+  border: none;
+  box-sizing: border-box;
+  & > svg {
+    color: gray;
+    flex: none;
+  }
+
+  input {
+    width: 100%;
+    border: none;
+    margin-left: 10px;
+    font-size: 17px;
+    outline: none;
+  }
+  button {
+    color: #ffffff;
+    background-color: #007be9;
+    border: 0;
+    cursor: pointer;
+    border-radius: 100%;
+    width: 55px;
+    height: 48px;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+  }
 `
 
 export default SearchBarContainer
