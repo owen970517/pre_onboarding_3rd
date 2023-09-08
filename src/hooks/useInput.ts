257c82f54fa03153d18b5668e9bfb4bbc04cf823
owen970517 <tamia123123@naver.com>
2023-09-08 19:33:32 +0900

    refactor: submit 코드를 버튼 클릭, 폼, 키보드로 수행 가능하도록 개선

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -4,29 +4,29 @@ import { searchActions } from "../stores/searchList";
 
 export const useInput = () => {
     const dispatch = useDispatch();
-    const { searchValue, nowIndex } = useSelector((state:RootState) => state.searchList)
+    const { searchValue } = useSelector((state:RootState) => state.searchList)
     const handleSearchValue = (value:string) => {
-        dispatch(searchActions.setSearchValue(value))
+      dispatch(searchActions.setSearchValue(value))
     }
     const handleInputFocus = () => {
-        dispatch(searchActions.setIsFocus(true));
+      dispatch(searchActions.setIsFocus(true));
     };
-    const handleInputBlur = () => {
-        dispatch(searchActions.setIsFocus(false));
-    };
-    const handleSearched = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        if (nowIndex < 0) {
-          dispatch(searchActions.setSearchedList(searchValue))
-          dispatch(searchActions.setSearchValue(''));
-          alert(`${searchValue}를 검색하셨습니다.`)
-        }
+    const handleSearched = (e?:React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLParagraphElement, MouseEvent> | KeyboardEvent, item?:string) => {
+      if (e && 'preventDefault' in e) {
+        e.preventDefault();
       }
+      const searchedValue = item ? item : searchValue
+      if (searchedValue !== '') {
+        dispatch(searchActions.setSearchedList(searchedValue))
+        dispatch(searchActions.setSearchValue(''));
+        alert(`${searchedValue}를 검색하셨습니다.`)
+      }
+    }
+
     return {
       searchValue,
       handleSearchValue,
       handleInputFocus,
-      handleInputBlur,
       handleSearched
     };
 };
